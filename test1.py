b2f57a7f440b72e11010dc56c3aaef35e69b5cb8
B佬君 <34976550+EmoBSAMA@users.noreply.github.com>
2018-06-18 20:31:02 +0800

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3 
+# -*- coding: utf-8 -*-
+
+chang=input('请你输入屏幕长度')
+kuan=input('请你输入屏幕宽度')
+
+chang=int(chang)
+kuan=int(kuan)
+
+print('像素点数目',chang*kuan)
---
+++ test4.py
@@ -0,0 +1,6 @@
+y=1
+for x in range(101):
+	if x>0:
+		y=y*x
+print(y)
+
